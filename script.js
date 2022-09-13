9d0d2fe83501dd85eb2277b316a4aa325f023a6d
Souvik Biswas <you@example.com>
2022-09-13 14:57:43 +0530

    done today

--- script.js
+++ script.js
@@ -33,3 +33,30 @@ const sr=ScrollReveal({
 sr.reveal(".home-text",{delay:190,origin:"bottom"})
 
 sr.reveal(".about,.services,.portfolio,.contact",{delay:200,origin:"bottom"})
+
+
+
+
+
+
+
+
+
+var tablinks=document.getElementsByClassName("tab-links");
+var tabcontents=document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+
+for(tablink of tablinks){
+    tablink.classList.remove("active-links")
+}
+
+for(tabcontent of tabcontents){
+    tabcontent.classList.remove("active-tab")
+}
+
+event.currentTarget.classList.add("active-link")
+
+document.getElementById(tabname).classList.add("active-tab")
+
+}
